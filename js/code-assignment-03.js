1ccfe2233d952e7ca5d2d4d43f33c55e1cec3a43
Krista <35709788+kristafmyers@users.noreply.github.com>
2018-02-14 13:16:31 -0600

    update

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -7,21 +7,21 @@ let projectThumb = true
 let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
 
 
-let projectId = 2
-let projectUrl = 'https://www.arcgis.com/apps/Minimalist/index.html?appid=22073ad7d34240b3acf80f0b1ec00929'
-let projectTitle = 'McMurdo LTER Meterological Station Network (Book 2)'
-let projectDesc = 'Map of active meterological stations and established camps throughout the Dry Valleys. Meteorological stations are color coded by January 2014 average air temperature. Click on each station for site specific information such as location, average air temperatures, data sources, and photographs. Created by Krista Myers 2018-01-31'
-let projectThumb = true
-let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
-
-
-let projectId = 3
-let projectUrl = 'https://web-gis-kfm.maps.arcgis.com/apps/GeoForm/index.html?appid=48d838a0c57d431bb1173b86ea8fb409'
-let projectTitle = 'Alaska Weather - Citizen Science Based Web Map'
-let projectDesc = 'Crowd sourced meterological data from across the state of Alaska'
-let projectThumb = true
-let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
-
+// let projectId = 2
+// let projectUrl = 'https://www.arcgis.com/apps/Minimalist/index.html?appid=22073ad7d34240b3acf80f0b1ec00929'
+// let projectTitle = 'McMurdo LTER Meterological Station Network (Book 2)'
+// let projectDesc = 'Map of active meterological stations and established camps throughout the Dry Valleys. Meteorological stations are color coded by January 2014 average air temperature. Click on each station for site specific information such as location, average air temperatures, data sources, and photographs. Created by Krista Myers 2018-01-31'
+// let projectThumb = true
+// let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
+//
+//
+// let projectId = 3
+// let projectUrl = 'https://web-gis-kfm.maps.arcgis.com/apps/GeoForm/index.html?appid=48d838a0c57d431bb1173b86ea8fb409'
+// let projectTitle = 'Alaska Weather - Citizen Science Based Web Map'
+// let projectDesc = 'Crowd sourced meterological data from across the state of Alaska'
+// let projectThumb = true
+// let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
+//
 
 // display messages in the browser's developer console, combining the values with text
 console.log('ID: ' + projectId)
