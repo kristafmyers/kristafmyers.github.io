8def22e2a4b2b00a370e1ee7692196b52339fa5f
kristafmyers <35709788+kristafmyers@users.noreply.github.com>
2018-04-10 11:19:23 -0500

    Webmap5

---
+++ map/webmap-assignment-5.js
@@ -0,0 +1,29 @@
+
+//let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
+//this is two different "functions" immediately one after another - called chaining
+function map5 () {
+let webmap = L.map('webmap5').setView([63.843414, -150.405056],4)
+let satelliteBase= L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(webmap)
+
+let topoBase=L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(webmap)
+
+let boringMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(webmap)
+
+let myThreeBasemaps = {
+	'Satellite imagery basemap': satelliteBase,
+	'Topography basemap': topoBase,
+	'Street basemap': boringMap
+}
+
+L.control.layers(myThreeBasemaps).addTo(webmap)
+}
+
+map5()
